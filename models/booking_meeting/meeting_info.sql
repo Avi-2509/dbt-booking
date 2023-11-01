83bb976ff2ce4d97361366aaba9d625ef41b35b9
avisrivastava <avi.srivastava@lybl.com>
2023-11-01 10:50:49 +0530

    added userid and profile id

--- models/booking_meeting/meeting_info.sql
+++ models/booking_meeting/meeting_info.sql
@@ -3,14 +3,21 @@
 {% endset %}
 {% do run_query(query)%}
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 select
+        mc.userid ,
+        mc.profileid ,
+        mcp.defaultrole as "createdBy",
+        mcp."name" ,
 	m.slot."from",
 	m.slot."to",
 	m.meetingjoinstateenum,
-    m.meetingstatus,
-    dis."name",
-    md.modalitytype
-    FROM booking.meeting m, booking.meeting_modality md, booking.meeting_disease dis
-    where m."_airbyte_meeting_hashid" = md."_airbyte_meeting_hashid" and 
-    dis."_airbyte_meeting_hashid" = m."_airbyte_meeting_hashid"
+        m.meetingstatus,
+        dis."name" as "disease",
+        md.modalitytype
+        FROM booking.meeting m, booking.meeting_modality md, booking.meeting_disease dis, 
+        booking.meeting_createdby mc, booking.meeting_createdby_profile mcp
+        where m."_airbyte_meeting_hashid" = md."_airbyte_meeting_hashid" and 
+        dis."_airbyte_meeting_hashid" = m."_airbyte_meeting_hashid" and 
+        mc._airbyte_meeting_hashid = m._airbyte_meeting_hashid and 
+        mcp._airbyte_createdby_hashid = mc._airbyte_createdby_hashid 
