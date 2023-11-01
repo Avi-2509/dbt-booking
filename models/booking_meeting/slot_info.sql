f9ec9894070e001d587d7a95c08fe2e6080d74c8
avisrivastava <avi.srivastava@lybl.com>
2023-11-01 11:31:19 +0530

    added slot info sql

---
+++ models/booking_meeting/slot_info.sql
@@ -0,0 +1,25 @@
+{% set query %}
+        set enable_case_sensitive_identifier to true
+{% endset %}
+{% do run_query(query)%}
+
+{{ config(materialized='view') }}
+select tup.userid as "doctoruserid" , 
+tup.profileid as "doctorprofileid",
+t.validity."from" as "slotstart",
+t.validity."to" as "slotend",
+tdmap.monday, 
+tdmap.tuesday , 
+tdmap.wednesday , 
+tdmap.thursday , 
+tdmap.friday , 
+tdmap.saturday, 
+tdmap.sunday ,
+t.timetableenum
+from booking.timetable t,
+booking.timetable_dayavailabilitymap tdmap,
+booking.timetable_userprofile tup,
+booking.timetable_userprofile_profile tuprof 
+where t._airbyte_timetable_hashid = tdmap._airbyte_timetable_hashid and 
+tup._airbyte_timetable_hashid = t._airbyte_timetable_hashid and 
+tup._airbyte_userprofile_hashid = tuprof._airbyte_userprofile_hashid 
